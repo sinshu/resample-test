fd0654c7c05a25cb031bd40a0e296e4e0218141a
Nobuaki Tanaka <sinshu.d@gmail.com>
2026-07-18 13:30:06 +0900

    a

---
+++ NewNumFlat/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using NumFlat;
+using NumFlat.IO;
+using NumFlat.SignalProcessing;
+
+public static class Program
+{
+    private static readonly int highSampleRate = 48000;
+    private static readonly int lowSampleRate = 16000;
+
+    private static readonly int sourceSignalLength = 60 * highSampleRate;
+
+    public static void Main(string[] args)
+    {
+        var random = new Random(42);
+        var signal = new Vec<double>(sourceSignalLength);
+        foreach (ref var value in signal)
+        {
+            value = random.NextDouble() - 0.5;
+        }
+
+        var sw = new Stopwatch();
+
+        sw.Start();
+        var resampled = signal.Resample(1, 3, 50);
+        sw.Stop();
+
+        WaveFile.Write("source.wav", signal, highSampleRate);
+        WaveFile.Write("resampled.wav", resampled, lowSampleRate);
+
+        Console.WriteLine(sw.ElapsedMilliseconds);
+    }
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,17 @@
+﻿using Zx;
+
+var config = Path.GetDirectoryName(AppContext.BaseDirectory);
+config = Path.GetDirectoryName(config);
+config = Path.GetFileName(config);
+
+var root = Path.GetDirectoryName(AppContext.BaseDirectory);
+root = Path.GetDirectoryName(root);
+root = Path.GetDirectoryName(root);
+root = Path.GetDirectoryName(root);
+root = Path.GetDirectoryName(root);
+
+var oldNumFlatPath = Path.Combine(root, "OldNumFlat", "bin", config, "net10.0", "OldNumFlat.exe");
+await oldNumFlatPath;
+
+var newNumFlatPath = Path.Combine(root, "NewNumFlat", "bin", config, "net10.0", "NewNumFlat.exe");
+await newNumFlatPath;
